bc011044818e5f385ad94562a57b2f814ceec0d3
Taha Shakibania <shakibaniataha@gmail.com>
2017-11-29 11:28:06 +0330

    User signup added.

--- main/views.py
+++ main/views.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from main.forms import SignUpForm
 
 def index(request):
     return render(request, 'main/index.html', {})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'main/signup.html', {'form': form})
