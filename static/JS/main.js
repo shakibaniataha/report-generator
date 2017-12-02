51c5763157a6e7e110a526197c40dfbe3d01ba3a
Taha Shakibania <shakibaniataha@gmail.com>
2017-12-02 18:15:53 +0330

    Added a run button at the end of each row

--- static/JS/main.js
+++ static/JS/main.js
@@ -6,23 +6,35 @@ $("#api-select").on('change', function(){
                 'api_id': $(this).val()
             },
             dataType: 'json',
-            success: function (data) {
+            success: function (result) {
                 var requests = $('#requests');
                 if(!requests.is(":visible")){
                     requests.show()
                 }
                 requests.DataTable({
-                    "aaData": data,
+                    "aaData": result,
                     "aoColumns": [
                         { "mDataProp": "id" },
                         { "mDataProp": "api_name" },
                         { "mDataProp": "preferred_run_date" },
                         { "mDataProp": "params" },
-                        { "mDataProp": "status" }
+                        { "mDataProp": "status" },
+                        {
+                            data: 'id',
+                            render: function (data) {
+                                return '<input type="button" id="'+data+'" value="Run" class="script-run-btn"/>';
+                            }
+                        }
+
                     ],
                     destroy: true
                 });
             }
+        })
+        .done(function(){
+            $('.script-run-btn').on('click', function(){
+                alert(this.id);
+            });
         });
     }
 });
